b3b2e8edff2dd1205b46ef0e0a568d6d86cb76ca
Ash <ashley@getdarker.com>
2013-05-28 16:08:37 +0100

    Heartbeat 6

--- lib/heartbeat.php
+++ lib/heartbeat.php
@@ -1,14 +1,18 @@
 <?php
 
-	$heartbeat = http_build_query(array("data" => json_encode($guano)));
-	$opts = array('http' =>
-		array(
-			'method'  => 'POST',
-			'header'  => 'Content-type: application/x-www-form-urlencoded',
-			'content' => $heartbeat
-		)
-	);
-	$context  = stream_context_create($opts);
-	file_get_contents("http://guano.im/hb.php?data=".$heartbeat, false, $context);
+	if ($guano['heartbeat']) {
+
+		$heartbeat = http_build_query(array("data" => json_encode($guano)));
+		$opts = array('http' =>
+			array(
+				'method'  => 'POST',
+				'header'  => 'Content-type: application/x-www-form-urlencoded',
+				'content' => $heartbeat
+			)
+		);
+		$context  = stream_context_create($opts);
+		file_get_contents("http://guano.im/hb.php?data=".$heartbeat, false, $context);
+		
+	}
 
 ?>
