80b49c484de74f337764a6ff1d7c46f46d0e808a
Ash <ashley@getdarker.com>
2013-05-28 11:28:18 +0100

    Import.

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Guano - a small set of scripts that helps you build an audience on Twitter, auto
 
 Guano runs off of the already established 'team follow back' process that already exists within twitter and thrives off of other peoples vanity.
 
-Guano isn't recommended for personal accounts.
+Guano isn't recommended for personal Twitter accounts.
---
+++ lib/updater.php
@@ -0,0 +1,17 @@
+<?php
+
+	if ($guano['autoUpdate'] || !isset($guano['autoUpdate'])) {
+		if (file_exists("version.json")) {
+			$guano['currentVersions'] = json_decode(file_get_contents("version.json"), true);
+		}
+		file_put_contents("version.json", file_get_contents("https://raw.github.com/zivc/Guano/master/version.json"));
+		$guano['updatedVersions'] = json_decode(file_get_contents("version.json"), true);
+		foreach ($guano['updatedVersions'] AS $k => $v) {
+			if (!isset($guano['currentVersions'][$k]['version']) || $guano['currentVersions'][$k]['version'] < $guano['updatedVersions'][$k]['version']) {
+				echo "Updating ".$k." to version ".$guano['updatedVersions'][$k]['version']."..\n";
+				file_put_contents("lib/".$k.".php", file_get_contents($guano['updatedVersions'][$k]['path']));
+			}
+		}
+	}
+
+?>
